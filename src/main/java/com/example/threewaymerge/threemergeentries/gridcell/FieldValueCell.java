0bd235af1b138bf4a2144148d84d6286fa2bd267
Houssem Nasri <housi.housi2015@gmail.com>
2022-06-21 18:59:29 +0100

    Fix scrolling bug & mouse cursor & text selection

--- src/main/java/com/example/threewaymerge/threemergeentries/gridcell/FieldValueCell.java
+++ src/main/java/com/example/threewaymerge/threemergeentries/gridcell/FieldValueCell.java
@@ -3,9 +3,12 @@ package com.example.threewaymerge.threemergeentries.gridcell;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.BooleanPropertyBase;
 import javafx.css.PseudoClass;
+import javafx.event.Event;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -13,8 +16,14 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Ellipse;
 
 import org.fxmisc.richtext.StyleClassedTextArea;
+import org.fxmisc.wellbehaved.event.InputMap;
+import org.fxmisc.wellbehaved.event.Nodes;
 import org.kordamp.ikonli.fontawesome5.FontAwesomeSolid;
 import org.kordamp.ikonli.javafx.FontIcon;
+
+import static org.fxmisc.wellbehaved.event.EventPattern.anyOf;
+import static org.fxmisc.wellbehaved.event.EventPattern.eventType;
+import static org.fxmisc.wellbehaved.event.EventPattern.mousePressed;
 
 public class FieldValueCell extends AbstractFieldCell {
 
@@ -23,8 +32,7 @@ public class FieldValueCell extends AbstractFieldCell {
     private final StyleClassedTextArea textLabel = new StyleClassedTextArea();
 
     public void setTextLabelBold() {
-        textLabel.setStyle("-fx-font-weight: bold;-fx-text-fill: #313131");
-
+        textLabel.setStyle("-fx-font-weight: bold;-fx-text-fill: #313131;-fx-background-color: #0000;-fx-cursor: hand");
     }
 
     public FieldValueCell(String text) {
@@ -47,16 +55,34 @@ public class FieldValueCell extends AbstractFieldCell {
     }
 
     private void initializeTextLabel() {
+        textLabel.getStyleClass().add("text-area-class");
         textLabel.setEditable(false);
         textLabel.setStyle("-fx-background-color: #0000");
         textLabel.appendText(textProperty().get());
         textLabel.setAutoHeight(true);
-        textLabel.setCursor(Cursor.HAND);
 
-        /*textLabel.textProperty().bind(textProperty());*/
         textLabel.setMaxWidth(Double.MAX_VALUE);
         textLabel.setWrapText(true);
         HBox.setHgrow(textLabel, Priority.ALWAYS);
+
+        textLabel.addEventFilter(ScrollEvent.SCROLL, e -> {
+            e.consume();
+            FieldValueCell.this.fireEvent(e.copyFor(e.getSource(), FieldValueCell.this));
+        });
+
+        InputMap<Event> preventSelectionOrRightArrowNavigation = InputMap.consume(
+                anyOf(
+                        // prevent selection via mouse events
+                        eventType(MouseEvent.MOUSE_DRAGGED),
+                        eventType(MouseEvent.DRAG_DETECTED),
+                        eventType(MouseEvent.MOUSE_ENTERED),
+                        mousePressed().unless(e -> e.getClickCount() == 1)
+                )
+        );
+        Nodes.addInputMap(textLabel, preventSelectionOrRightArrowNavigation);
+
+        textLabel.setStyle("-fx-background-color: #0000;-fx-cursor: hand");
+
     }
 
     private void initializeSelectionBox() {
@@ -102,7 +128,6 @@ public class FieldValueCell extends AbstractFieldCell {
     public boolean isSelected() {
         return selectedProperty().get();
     }
-
 
     public BooleanProperty selectedProperty() {
         return selected;
